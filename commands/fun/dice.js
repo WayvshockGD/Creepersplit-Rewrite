4fcdb92160f2cd71ade45007ab3f5025dab2e5f3
Irian3x3 <irian3x3@gmail.com>
2020-11-19 09:32:57 -0400

    Updated dice; No need to make a variable for numbers

--- commands/fun/dice.js
+++ commands/fun/dice.js
@@ -5,22 +5,10 @@ module.exports = {
     usage: 'dice',
     async run(message, args, bot) {
 
-        var numbers = [
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9"
-        ]
-
         const m = await message.channel.send('Rolling...')
 
         setTimeout(() => {
-            m.edit(`You Rolled a \`${numbers[Math.floor(Math.random() * numbers.length)]}\`!`)
+            m.edit(`You Rolled a \`${Math.floor(Math.random() * 10)}\`!`)
         }, 1500);
 
     }
